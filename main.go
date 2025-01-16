6efd6e6e940b51c3a04240b2fc6cd21587c15279
aire1 <aire1@protonmail.com>
2025-01-16 23:28:32 +0300

    refactored main function to enhance page fetching and parsing logic

--- main.go
+++ main.go
@@ -1,13 +1,83 @@
 package main
 
 import (
-	"kodik_parser/utils"
+	"fmt"
+	"log"
 	"net/http"
+
+	"kodik_parser/utils" // Замените на актуальный путь
 )
 
 func main() {
 	client := &http.Client{}
+	defer client.CloseIdleConnections()
 
-	responseBody := utils.GetKodikPage(client, "https://kodik.online/serial/42283/nymqLn7fa89e31")
+	// Инициализация параметров
+	params := utils.KodikParams{}
+	url := "https://kodik.online/serial/42283/nymqLn7fa89e31"
 
+	// Получаем домен
+	domain, err := utils.ParseDomainFromURL(url)
+	if err != nil {
+		log.Fatalf("Error parsing domain from URL: %v", err)
+	}
+	params.MainDomain.Domain = domain
+
+	// Получаем страницу
+	responseBody, err := utils.GetPage(client, url, utils.KodikPage.MAIN_PAGE, &params, "")
+	if err != nil {
+		log.Fatalf("Error getting page: %v", err)
+	}
+
+	// Извлекаем URL iframe
+	playerPageURL, err := utils.ParseIframeURL(responseBody)
+	if err != nil {
+		log.Fatalf("Error parsing iframe URL: %v", err)
+	}
+
+	// Получаем страницу плеера
+	responseBody, err = utils.GetPage(client, playerPageURL, utils.KodikPage.PLAYER_PAGE, &params, "")
+	if err != nil {
+		log.Fatalf("Error getting player page: %v", err)
+	}
+
+	// Парсим параметры из страницы плеера
+	err = utils.ParseURLParameters(responseBody, &params)
+	if err != nil {
+		log.Fatalf("Error parsing URL parameters: %v", err)
+	}
+
+	// // Извлекаем детали сериала
+	// serialDetails, err := utils.ParseSerialDetails(responseBody)
+	// if err != nil {
+	// 	log.Fatalf("Error parsing serial details: %v", err)
+	// }
+
+	// Извлекаем видео информацию
+	seasonSeries, err := utils.ParseSeasonSeries(responseBody)
+	if err != nil {
+		log.Fatalf("Error parsing seasonSeries: %v", err)
+	}
+
+	// // Получаем URL для скрипта сериала
+	// appSerialURL, err := utils.GetSerialScriptURL(responseBody, params.PlayerDomain.Domain)
+	// if err != nil {
+	// 	log.Fatalf("Error getting serial script URL: %v", err)
+	// }
+
+	// // Получаем скрипт сериала
+	// responseBody, err = utils.GetPage(client, appSerialURL, utils.KodikPage.APP_SERIAL_SCRIPT, &params, playerPageURL)
+	// if err != nil {
+	// 	log.Fatalf("Error getting app serial script: %v", err)
+	// }
+
+	// // Извлекаем секретный метод
+	// secretMethod, err := utils.GetSecretMethod(responseBody)
+	// if err != nil {
+	// 	log.Fatalf("Error getting secret method: %v", err)
+	// }
+
+	// Печатаем результаты для проверки
+	fmt.Println("Serial series:", seasonSeries)
+	//fmt.Println("Secret Method:", secretMethod)
 }
